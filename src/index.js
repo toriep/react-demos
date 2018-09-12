750518b6ca961b20e22756d800a2228c16e86361
Torie Pham <tientp@uci.edu>
2018-09-12 16:43:04 -0700

    React intro challenge complete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const user = {
+    firstName: 'Torie',
+    lastName: 'Pham'
+};
+
+function fullName(user){
+    return `${user.firstName} ${user.lastName}`;
+}
+
+const greeting = <h1>Hello {fullName(user)}!</h1>;
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
