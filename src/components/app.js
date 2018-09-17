ebefaa3ba4d6d779200b82412663ecbc45d5e0b9
Torie Pham <tientp@uci.edu>
2018-09-17 14:32:52 -0700

    input form with react

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import DemoForm from "./demo_form";
+
+const user = {
+    email: "anemail@latest.com"
+}
 
 const App = () => (
     <div>
         <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
+            <DemoForm email={user.email}/>
         </div>
     </div>
 );
